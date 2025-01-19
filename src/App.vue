593b648cee704043418e0c785b9c394c2a32f155
cendyz <arek57823@gmail.com>
2025-01-19 09:08:06 +0100

    mobile design done

--- src/App.vue
+++ src/App.vue
@@ -7,36 +7,101 @@ const DEFAULT_DATA = {
 }
 
 const newData = reactive(DEFAULT_DATA)
-
-console.log(newData.commentBox)
 </script>
 <template>
 	<main class="main">
 		<div
-			class="box"
+			class="mainBox"
 			v-for="(value, index) in newData.commentBox"
 			:key="value.id">
-			<div class="upperBox">
-				<img
-					:src="value.user.image.webp"
-					alt="Profile picture"
-					class="avatar" />
-				<p class="nick">{{ value.user.username }}</p>
-				<p class="time">{{ value.createdAt }}</p>
-			</div>
-			<p class="desc">
-				{{ value.content }}
-			</p>
-			<div class="bottomBox">
-				<div class="btnsBox">
-					<button class="leftBtn">+</button>
-					<p class="votes">{{ value.score }}</p>
-					<button class="rightBtn">-</button>
+			<div class="box">
+				<div class="upperBox">
+					<img
+						:src="value.user.image.webp"
+						alt="Profile picture"
+						class="avatar" />
+					<p class="nick">{{ value.user.username }}</p>
+					<p class="time">{{ value.createdAt }}</p>
 				</div>
-				<button class="reply">
-					<img src="./images/icon-reply.svg" alt="" class="replyIcon" />
-					Reply
-				</button>
+				<p class="desc">
+					{{ value.content }}
+				</p>
+				<div class="bottomBox">
+					<div class="btnsBox">
+						<button class="leftBtn">+</button>
+						<p class="votes">{{ value.score }}</p>
+						<button class="rightBtn">-</button>
+					</div>
+					<button class="reply">
+						<img src="./images/icon-reply.svg" alt="" class="replyIcon" />
+						Reply
+					</button>
+				</div>
+			</div>
+			<div
+				class="mainReplyBox"
+				v-if="newData.commentBox[index].replies.length > 0">
+				<div
+					class="insdieReplyBox"
+					v-for="(value, index) in newData.commentBox[index].replies"
+					:key="value.id">
+					<div class="upperBox">
+						<img
+							:src="value.user.image.webp"
+							alt="Profile picture"
+							class="avatar" />
+						<p class="nick">{{ value.user.username }}</p>
+						<p v-if="value.user.username === 'juliusomo'" class="you">
+							you
+						</p>
+						<p class="time">{{ value.createdAt }}</p>
+					</div>
+					<p class="desc">
+						<span class="calledUser">@{{ value.replyingTo }}</span>
+						{{ value.content }}
+					</p>
+					<div class="bottomBox">
+						<div class="btnsBox">
+							<button class="leftBtn">+</button>
+							<p class="votes">{{ value.score }}</p>
+							<button class="rightBtn">-</button>
+						</div>
+						<button
+							class="reply"
+							v-if="value.user.username !== 'juliusomo'">
+							<img
+								src="./images/icon-reply.svg"
+								alt=""
+								class="replyIcon" />
+							Reply
+						</button>
+						<div
+							class="ownButtons"
+							v-if="value.user.username == 'juliusomo'">
+							<button class="delete">
+								<img
+									src="./images/icon-delete.svg"
+									alt=""
+									class="deleteIcon" />
+								Delete
+							</button>
+							<button class="edit">
+								<img
+									src="./images/icon-edit.svg"
+									alt=""
+									class="editIcon" />
+								Reply
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="commentBox">
+			<textarea id="textarea" placeholder="Add a comment..." class="textarea"></textarea>
+			<div class="downBox">
+				<img src="./images/avatars/image-juliusomo.webp" alt="Your avatar" class="avatar">
+				<button class="send" type="button">send</button>
 			</div>
 		</div>
 	</main>
@@ -51,7 +116,6 @@ body {
 	font-family: 'Rubik', serif;
 	font-size: 1.6rem;
 	background-color: $very-light-gray;
-	background-color: #333;
 }
 
 .wrapper {
